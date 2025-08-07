1ab7111fce58c1d054ea711c2c441a5ddb52524d
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-08-07 14:42:12 +0900

    Add: backend db connect test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,30 @@
-import express from "express";
+import prisma from "@/lib/prisma";
 
-const app = express();
-const port = process.env.PORT || 300;
+const main = async () => {
+  try {
+    console.log("Create a user...");
+    const user = await prisma.user.create({
+      data: {
+        email: "test@example.com",
+        passwordHash: "hashed_password_for_test",
+        name: "Test User",
+      },
+    });
+    console.log("a user has been created:", user);
 
-app.use(express.json()); // Enable JSON body parsing
+    const fetchedUser = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+    console.log("ユーザーが見つかりました:", fetchedUser);
+  } catch (error) {
+    console.error("エラーが発生しました:", error);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+    console.log("データベース接続を切断しました。");
+  }
+};
 
-app.get("/", (req, res) => {
-  res.send("Hello from Sweet Sprouts Backend!");
-});
-
-app.listen(port, () => {
-  console.log(`Backend server running on http://localhost:${port}`);
-});
+main();

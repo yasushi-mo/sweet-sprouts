d66b6517d829b7df221d8b493a268beef7aa56ec
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-08-13 18:31:49 +0900

    Add: authMiddleware

--- backend/src/constants/index.ts
+++ backend/src/constants/index.ts
@@ -1,1 +1,5 @@
 export const SALT_ROUNDS = 10;
+
+export const JWT_SECRET = process.env.JWT_SECRET || "your-very-secret-key";
+export const JWT_REFRESH_SECRET =
+  process.env.JWT_REFRESH_SECRET || "your-very-secret-refresh-key";
